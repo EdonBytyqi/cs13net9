d8936db61ca707853d0c283b00e92b47d8824b81
EdonBytyqi <edonbytyqi@outlook.com>
2026-01-15 16:18:01 +0100

    FizzBuzzGame

---
+++ Chapter03/Excercise_LoopsAndOverflow/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int max = 500;
+
+//for (byte i = 0; i <= max; i++)
+//{
+//    Console.WriteLine(i);
+//}
+// The above code will cause an infinite loop due to byte overflow.
+// To fix this, we can change the loop variable to a larger data type like int.
+for (int i = 0; i <= max; i++)
+{
+    Console.WriteLine(i);
+}
---
+++ Chapter03/FizzBuzzGame/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string fizz = "Fizz";
+string buzz = "Buzz";
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine(fizz + buzz);
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine(fizz);
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(buzz);
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
