d16133df745c4e33f3c7b45d04cd14402a497b4d
EdonBytyqi <edonbytyqi@outlook.com>
2026-02-18 14:28:46 +0100

    Implementing functionality using methods

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -8,4 +8,38 @@ Person harry = new()
         offset: TimeSpan.Zero)
 };
 
+Person edon = new() { Name = "Edon" };
+Person donjeta = new() { Name = "Donjeta" };
+Person asddsa = new() { Name = "Asddsa" };
+
+//call instance method to marry two persons
+edon.Marry(donjeta);
+
+//static method
+edon.Marry(asddsa);
+
+edon.OutputSpouses();
+donjeta.OutputSpouses();
+asddsa.OutputSpouses();
+
+//call instance method to make a baby
+Person baby1 = Person.Procreate(edon, donjeta);
+baby1.Name = "Art";
+WriteLine($"{baby1.Name} was born on {baby1.Born}");
+
+Person baby2 = Person.Procreate(edon, asddsa);
+baby2.Name = "Art";
+WriteLine($"{baby2.Name} was born on {baby2.Born}");
+
+edon.WriteChildrenToConsole();
+donjeta.WriteChildrenToConsole();
+asddsa.WriteChildrenToConsole();
+
+for (int i = 0; i < edon.Children.Count; i++)
+{
+    WriteLine(format: "  {0}'s child #{1} is named \"{2}\".",
+    arg0: edon.Name, arg1: i,
+    arg2: edon.Children[i].Name);
+}
+
 harry.WriteToConsole();
