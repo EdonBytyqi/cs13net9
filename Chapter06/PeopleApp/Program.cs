0635588769a2ffb50dc937dfb13311ac417dac27
EdonBytyqi <edonbytyqi@outlook.com>
2026-02-18 13:35:09 +0100

    Setting up a class library and console application

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace PeopleApp;
+﻿using Packt.Shared;
 
-class Program
+Person harry = new()
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+    Name = "Harry",
+    Born = new(year: 2001, month: 3, day: 25,
+        hour: 0, minute: 0, second: 0,
+        offset: TimeSpan.Zero)
+};
+
+harry.WriteToConsole();
